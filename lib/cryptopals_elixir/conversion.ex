873d9b3ce9fb1a120ae4ca16f271442e71828ee5
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 09:43:05 +1100

    working

--- lib/cryptopals_elixir/conversion.ex
+++ lib/cryptopals_elixir/conversion.ex
@@ -1,3 +1,5 @@
 defmodule CryptopalsElixir.Conversion do
-
+  def hex_to_bytes(_) do
+    <<0 :: size(8)>>
+  end
 end
--- test/cryptopals_elixir/conversion_test.exs
+++ test/cryptopals_elixir/conversion_test.exs
@@ -1,4 +1,11 @@
 defmodule CryptopalsElixir.ConversionTest do
   use ExUnit.Case
   doctest CryptopalsElixir.Conversion
+
+  test "converting a single char to bytes" do
+    actual = CryptopalsElixir.Conversion.hex_to_bytes("0")
+    expected =  <<0 :: size(8)>>
+
+    assert(actual == expected)
+  end
 end
