1083508ac8ae5feb9ff893ac0dd0d292acdd2149
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 09:53:21 +1100

    working

--- lib/cryptopals_elixir/conversion.ex
+++ lib/cryptopals_elixir/conversion.ex
@@ -3,7 +3,7 @@ defmodule CryptopalsElixir.Conversion do
   def hex_to_bytes("1"), do: <<1::size(8)>>
   def hex_to_bytes("2"), do: <<2::size(8)>>
 
-  def hex_to_bytes(char) when is_list(char) and length(char) == 1 do
+  def hex_to_bytes(char) when is_binary(char) and byte_size(char) == 1 do
     {int, _} = Integer.parse(char)
     <<int::size(8)>>
   end
