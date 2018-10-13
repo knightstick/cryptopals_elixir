5de0b05837b7aedee607ef6da9b1555f052d2121
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 10:13:21 +1100

    working

--- lib/cryptopals_elixir/conversion.ex
+++ lib/cryptopals_elixir/conversion.ex
@@ -1,7 +1,7 @@
 defmodule CryptopalsElixir.Conversion do
   def hex_to_bytes(char) when is_binary(char) and byte_size(char) == 1 do
     case Integer.parse(char, 16) do
-      {int, _} -> <<int::size(8)>>
+      {int, _} -> <<int::8>>
       :error -> raise ArgumentError
     end
   end
