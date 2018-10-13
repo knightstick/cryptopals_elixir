d4b11ccc372f6032871fdd3582bc93e83e13a232
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 10:52:46 +1100

    working

--- test/cryptopals_elixir/conversion_test.exs
+++ test/cryptopals_elixir/conversion_test.exs
@@ -2,22 +2,24 @@ defmodule CryptopalsElixir.ConversionTest do
   use ExUnit.Case
   doctest CryptopalsElixir.Conversion
 
+  alias CryptopalsElixir.Conversion
+
   test "converting a single char to bytes" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("0")
+    actual = Conversion.hex_to_bytes("0")
     expected = <<0::8>>
 
     assert(actual == expected)
   end
 
   test "converting a 1 to bytes" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("1")
+    actual = Conversion.hex_to_bytes("1")
     expected = <<1::8>>
 
     assert(actual == expected)
   end
 
   test "converting a 2 to bytes" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("2")
+    actual = Conversion.hex_to_bytes("2")
     expected = <<2::8>>
 
     assert(actual == expected)
@@ -25,61 +27,61 @@ defmodule CryptopalsElixir.ConversionTest do
 
   test "raises an error if the string is not hex" do
     assert_raise(ArgumentError, fn ->
-      CryptopalsElixir.Conversion.hex_to_bytes("x")
+      Conversion.hex_to_bytes("x")
     end)
   end
 
   test "converting an a to a byte" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("a")
+    actual = Conversion.hex_to_bytes("a")
     expected = <<10::8>>
 
     assert(actual == expected)
   end
 
   test "converting an f to a byte" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("f")
+    actual = Conversion.hex_to_bytes("f")
     expected = <<15::8>>
 
     assert(actual == expected)
   end
 
   test "converting two 0s to bytes" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("00")
+    actual = Conversion.hex_to_bytes("00")
     expected = <<0::8, 0::8>>
 
     assert(actual == expected)
   end
 
   test "converting a 0 and a 1" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("01")
+    actual = Conversion.hex_to_bytes("01")
     expected = <<0::8, 1::8>>
 
     assert(actual == expected)
   end
 
   test "converting a 1 and a 1" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("11")
+    actual = Conversion.hex_to_bytes("11")
     expected = <<1::8, 1::8>>
 
     assert(actual == expected)
   end
 
   test "converting a 2 and a b" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("2b")
+    actual = Conversion.hex_to_bytes("2b")
     expected = <<2::8, 11::8>>
 
     assert(actual == expected)
   end
 
   test "converting 3 char string" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("abc")
+    actual = Conversion.hex_to_bytes("abc")
     expected = <<10::8, 11::8, 12::8>>
 
     assert(actual == expected)
   end
 
   test "a blank string" do
-    actual = CryptopalsElixir.Conversion.hex_to_bytes("")
+    actual = Conversion.hex_to_bytes("")
     expected = <<>>
 
     assert(actual == expected)
