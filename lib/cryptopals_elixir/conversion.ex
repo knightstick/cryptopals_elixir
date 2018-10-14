5dc8efa42b4db18b0288499c32ca4b135485f9a2
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 15:19:30 +1100

    working

--- lib/cryptopals_elixir/conversion.ex
+++ lib/cryptopals_elixir/conversion.ex
@@ -19,24 +19,4 @@ defmodule CryptopalsElixir.Conversion do
       <<(<<bitstring::bitstring>>), (<<bits::4>>)>>
     end)
   end
-
-  def hex_to_bytes(char) when is_binary(char) and byte_size(char) == 1 do
-    case Integer.parse(char, 16) do
-      {int, _} -> <<int::8>>
-      :error -> raise ArgumentError
-    end
-  end
-
-  def hex_to_bytes(string) when is_binary(string) do
-    string
-    |> String.codepoints()
-    |> codepoints_to_bytes()
-  end
-
-  def codepoints_to_bytes(list) when is_list(list) do
-    list
-    |> Enum.reduce(<<>>, fn codepoint, bytes ->
-      bytes <> hex_to_bytes(codepoint)
-    end)
-  end
 end
