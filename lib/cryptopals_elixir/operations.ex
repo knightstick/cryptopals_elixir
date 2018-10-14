bd97d16349cd5e79359df0d5c9b09d062971dc79
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 18:45:35 +1100

    working

--- lib/cryptopals_elixir/operations.ex
+++ lib/cryptopals_elixir/operations.ex
@@ -1,2 +1,3 @@
 defmodule CryptopalsElixir.Operations do
+  def fixed_xor(_input_1, _input_2), do: <<0::1>>
 end
--- test/cryptopals_elixir/operations_test.exs
+++ test/cryptopals_elixir/operations_test.exs
@@ -1,4 +1,11 @@
 defmodule CryptopalsElixir.OperationsTest do
   use ExUnit.Case
   doctest CryptopalsElixir.Operations
+
+  describe "fixed_xor" do
+    actual = CryptopalsElixir.Operations.fixed_xor(<<0::1>>, <<0::1>>)
+    expected = <<0::1>>
+
+    assert(actual == expected)
+  end
 end
