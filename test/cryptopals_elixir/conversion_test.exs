ce000952f205e5943a7065fe1553e61450d4bf67
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-14 15:22:55 +1100

    working

--- test/cryptopals_elixir/conversion_test.exs
+++ test/cryptopals_elixir/conversion_test.exs
@@ -4,54 +4,54 @@ defmodule CryptopalsElixir.ConversionTest do
 
   alias CryptopalsElixir.Conversion
 
-  describe "hex_to_bitstring" do
+  describe "converting hex_to_bitstring" do
     def assert_converts_hex_to_bitstring(string, bitstring) do
       assert(Conversion.hex_to_bitstring(string) == bitstring)
     end
 
-    test "converting a single char to bitstring" do
+    test "0 returns 0" do
       assert_converts_hex_to_bitstring("0", <<0::4>>)
     end
 
-    test "converting a 1 to bitstring" do
+    test "1 returns 1" do
       assert_converts_hex_to_bitstring("1", <<1::4>>)
     end
 
-    test "converting a 2 to bitstring" do
+    test "2 returns 2" do
       assert_converts_hex_to_bitstring("2", <<2::4>>)
     end
 
-    test "raises an error if the string is not hex" do
+    test "non-hex string raises error" do
       assert_raise(ArgumentError, fn ->
         Conversion.hex_to_bitstring("x")
       end)
     end
 
-    test "converting an a to a bitstring" do
+    test "a returns 10" do
       assert_converts_hex_to_bitstring("a", <<10::4>>)
     end
 
-    test "converting an f to a bitstring" do
+    test "f returns 15" do
       assert_converts_hex_to_bitstring("f", <<15::4>>)
     end
 
-    test "2 0s" do
+    test "2 0s returns 0 0" do
       assert_converts_hex_to_bitstring("00", <<0::4, 0::4>>)
     end
 
-    test "a 0 and a 1" do
+    test "a 0 and a 1 returns 0 1" do
       assert_converts_hex_to_bitstring("01", <<0::4, 1::4>>)
     end
 
-    test "converting a 1 and a 1" do
+    test "1 and 1 returns 1 1" do
       assert_converts_hex_to_bitstring("11", <<1::4, 1::4>>)
     end
 
-    test "converting a 2 and a b" do
+    test "2 and b returns 2 11" do
       assert_converts_hex_to_bitstring("2b", <<2::4, 11::4>>)
     end
 
-    test "converting 3 char string" do
+    test "abc returns 10 11 12" do
       assert_converts_hex_to_bitstring("abc", <<10::4, 11::4, 12::4>>)
     end
 
