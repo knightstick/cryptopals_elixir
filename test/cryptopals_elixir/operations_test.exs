16a9da2664d9974b5674db0656348a8e8880f716
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-15 21:03:07 +1100

    working

--- test/cryptopals_elixir/operations_test.exs
+++ test/cryptopals_elixir/operations_test.exs
@@ -15,18 +15,11 @@ defmodule CryptopalsElixir.OperationsTest do
     end
 
     test "one 0 and one 1" do
-      actual = CryptopalsElixir.Operations.fixed_xor(<<0::1>>, <<1::1>>)
-      expected = <<1::1>>
-
-      assert(actual == expected)
       assert_fixed_xor_eq({<<0::1>>, <<1::1>>}, <<1::1>>)
     end
 
     test "one 1 and one 0" do
-      actual = CryptopalsElixir.Operations.fixed_xor(<<1::1>>, <<0::1>>)
-      expected = <<1::1>>
-
-      assert(actual == expected)
+      assert_fixed_xor_eq({<<1::1>>, <<0::1>>}, <<1::1>>)
     end
 
     test "two ones" do
