d5dd4c832a45441ebd8ec2fb045a912db29f8a62
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-15 21:17:51 +1100

    working

--- lib/cryptopals_elixir/operations.ex
+++ lib/cryptopals_elixir/operations.ex
@@ -4,11 +4,6 @@ defmodule CryptopalsElixir.Operations do
   def fixed_xor(<<1::1>>, <<0::1>>), do: <<1::1>>
   def fixed_xor(<<1::1>>, <<1::1>>), do: <<0::1>>
 
-  def fixed_xor(<<head_1::1, tail_1::1>>, <<head_2::1, tail_2::1>>) do
-    <<fixed_xor(<<head_1::1>>, <<head_2::1>>)::bitstring,
-      fixed_xor(<<tail_1::1>>, <<tail_2::1>>)::bitstring>>
-  end
-
   def fixed_xor(<<head_1::1, tail_1::bitstring>>, <<head_2::1, tail_2::bitstring>>) do
     <<fixed_xor(<<head_1::1>>, <<head_2::1>>)::bitstring,
       fixed_xor(<<tail_1::bitstring>>, <<tail_2::bitstring>>)::bitstring>>
