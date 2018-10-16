b09040395f7460b0f0b79dd6e8f8f863c6a42893
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 08:31:19 +1100

    working

--- lib/cryptopals_elixir/encryption.ex
+++ lib/cryptopals_elixir/encryption.ex
@@ -5,7 +5,7 @@ defmodule CryptopalsElixir.Encryption do
     do: Operations.fixed_xor(<<input::8>>, <<key::8>>)
 
   def xor_cypher(<<head::8, tail::bitstring>>, <<key::8>>) do
-    <<new_tail::bitstring>> = xor_cypher(<<tail::bitstring>>, <<key::8>>)
-    <<(<<Operations.fixed_xor(<<head::8>>, <<key::8>>)::bitstring>>), (<<new_tail::bitstring>>)>>
+    <<(<<Operations.fixed_xor(<<head::8>>, <<key::8>>)::bitstring>>),
+      (<<xor_cypher(<<tail::bitstring>>, <<key::8>>)::bitstring>>)>>
   end
 end
