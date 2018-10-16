8622ae299b946ca3092a1cc273609912a134ed90
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 08:38:28 +1100

    working

--- lib/cryptopals_elixir/decryption.ex
+++ lib/cryptopals_elixir/decryption.ex
@@ -1,2 +1,5 @@
 defmodule CryptopalsElixir.Decryption do
+  defmodule EnglishScoring do
+    def letter_weight("e"), do: 12
+  end
 end
--- test/cryptopals_elixir/decryption_test.exs
+++ test/cryptopals_elixir/decryption_test.exs
@@ -3,4 +3,10 @@ defmodule CryptopalsElixir.DecryptionTest do
   doctest CryptopalsElixir.Decryption
 
   alias CryptopalsElixir.Decryption
+
+  describe "letter_weights" do
+    test "e scores best" do
+      assert(Decryption.EnglishScoring.letter_weight("e") == 12)
+    end
+  end
 end
