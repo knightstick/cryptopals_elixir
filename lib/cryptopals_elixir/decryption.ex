3b9f36a4ef078a605322e849048ea8e929488c19
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 08:41:03 +1100

    working

--- lib/cryptopals_elixir/decryption.ex
+++ lib/cryptopals_elixir/decryption.ex
@@ -1,5 +1,10 @@
 defmodule CryptopalsElixir.Decryption do
   defmodule EnglishScoring do
     def letter_weight("e"), do: 12
+    def letter_weight("t"), do: 11
+    def letter_weight("a"), do: 10
+    def letter_weight("o"), do: 9
+    def letter_weight("i"), do: 8
+    def letter_weight("n"), do: 7
   end
 end
--- test/cryptopals_elixir/decryption_test.exs
+++ test/cryptopals_elixir/decryption_test.exs
@@ -8,5 +8,14 @@ defmodule CryptopalsElixir.DecryptionTest do
     test "e scores best" do
       assert(Decryption.EnglishScoring.letter_weight("e") == 12)
     end
+
+    test "a few letters" do
+      actual =
+        "etaoin"
+        |> String.graphemes()
+        |> Enum.map(&Decryption.EnglishScoring.letter_weight/1)
+
+      assert actual == [12, 11, 10, 9, 8, 7]
+    end
   end
 end
