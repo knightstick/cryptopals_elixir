ac222ebbe0ab236866b487963b1b22015a846a54
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 08:09:58 +1100

    working

--- lib/cryptopals_elixir/encryption.ex
+++ lib/cryptopals_elixir/encryption.ex
@@ -1,3 +1,6 @@
 defmodule CryptopalsElixir.Encryption do
+  def xor_cypher(<<input::8>>, <<key::8>>),
+    do: CryptopalsElixir.Operations.fixed_xor(<<input::8>>, <<key::8>>)
+
   def xor_cypher(input, key), do: CryptopalsElixir.Operations.fixed_xor(input, key)
 end
