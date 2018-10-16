e3cc120eaed80eda11f588d4b30c47d9f4317ea5
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 08:47:36 +1100

    working

--- lib/cryptopals_elixir/decryption.ex
+++ lib/cryptopals_elixir/decryption.ex
@@ -1,21 +1,19 @@
 defmodule CryptopalsElixir.Decryption do
   defmodule EnglishScoring do
-    def letter_weight("e"), do: 12
-    def letter_weight("t"), do: 11
-    def letter_weight("a"), do: 10
-    def letter_weight("o"), do: 9
-    def letter_weight("i"), do: 8
-    def letter_weight("n"), do: 7
-    def letter_weight("s"), do: 6
-    def letter_weight("h"), do: 5
-    def letter_weight("r"), do: 4
-    def letter_weight("d"), do: 3
-    def letter_weight("l"), do: 2
-    def letter_weight("u"), do: 1
-
     def letter_weight(char) when is_binary(char) and byte_size(char) == 1 do
       case String.upcase(char) do
         "E" -> 12
+        "T" -> 11
+        "A" -> 10
+        "O" -> 9
+        "I" -> 8
+        "N" -> 7
+        "S" -> 6
+        "H" -> 5
+        "R" -> 4
+        "D" -> 3
+        "L" -> 2
+        "U" -> 1
         _ -> 0
       end
     end
