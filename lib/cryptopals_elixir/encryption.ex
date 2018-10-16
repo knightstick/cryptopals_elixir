959263d2d259a7db948a47a223fdb99ea83b6c4d
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-16 20:39:43 +1100

    working

--- lib/cryptopals_elixir/encryption.ex
+++ lib/cryptopals_elixir/encryption.ex
@@ -1,3 +1,3 @@
 defmodule CryptopalsElixir.Encryption do
-  def xor_cypher(<<?a::8>>, <<0::8>>), do: <<?a::8>>
+  def xor_cypher(<<byte::8>>, <<0::8>>), do: <<byte::8>>
 end
--- test/cryptopals_elixir/encryption_test.exs
+++ test/cryptopals_elixir/encryption_test.exs
@@ -5,11 +5,19 @@ defmodule CryptopalsElixir.EncryptionTest do
   alias CryptopalsElixir.Encryption
 
   describe "xor_cypher" do
-    test "encrypting one byte with 0" do
+    test "1 byte against 0" do
       input = <<?a::8>>
       xor_key = <<0::8>>
       actual = Encryption.xor_cypher(input, xor_key)
       expected = <<?a::8>>
+      assert(actual == expected)
+    end
+
+    test "another byte against 0" do
+      input = <<?z::8>>
+      xor_key = <<0::8>>
+      actual = Encryption.xor_cypher(input, xor_key)
+      expected = <<?z::8>>
       assert(actual == expected)
     end
   end
