a0a910ae954e2db73eaf7a7cf6abf44525871bdd
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 20:45:38 +1100

    working

--- lib/cryptopals_elixir/decryption.ex
+++ lib/cryptopals_elixir/decryption.ex
@@ -1,15 +1,17 @@
 defmodule CryptopalsElixir.Decryption do
+  alias CryptopalsElixir.{Decryption, Encryption}
+
   def score_xor_key_likelihoods(encrypted_string) do
     %{
       <<0::8>> =>
-        CryptopalsElixir.Decryption.EnglishScoring.score(
+        Decryption.EnglishScoring.score(
           encrypted_string
-          |> CryptopalsElixir.Encryption.xor_cypher(<<0::8>>)
+          |> Encryption.xor_cypher(<<0::8>>)
         ),
       <<1::8>> =>
-        CryptopalsElixir.Decryption.EnglishScoring.score(
+        Decryption.EnglishScoring.score(
           encrypted_string
-          |> CryptopalsElixir.Encryption.xor_cypher(<<1::8>>)
+          |> Encryption.xor_cypher(<<1::8>>)
         )
     }
   end
