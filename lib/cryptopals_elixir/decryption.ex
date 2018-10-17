faf0e9f517e23ac4912bd2c840a56967491c7266
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-17 21:01:48 +1100

    working

--- lib/cryptopals_elixir/decryption.ex
+++ lib/cryptopals_elixir/decryption.ex
@@ -3,10 +3,14 @@ defmodule CryptopalsElixir.Decryption do
   alias Decryption.EnglishScoring
 
   def score_xor_key_likelihoods(input) do
-    [<<0::8>>, <<1::8>>, <<250::8>>]
-    |> Enum.reduce(%{}, fn key, acc ->
+    Enum.reduce(all_byte_combinations(), %{}, fn key, acc ->
       Map.put(acc, key, score_xor_key(input, key))
     end)
+  end
+
+  defp all_byte_combinations do
+    0b00000000..0b11111111
+    |> Enum.map(fn x -> <<x::8>> end)
   end
 
   defp score_xor_key(string, key) do
