9c746fae08a2239d92118776c829e7fb153aabd8
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-19 13:45:01 +1100

    working

--- test/cryptopals_elixir/file_reader_test.exs
+++ test/cryptopals_elixir/file_reader_test.exs
@@ -3,11 +3,19 @@ defmodule CryptopalsElixir.FileReaderTest do
   doctest CryptopalsElixir.FileReader
 
   describe "reading hex strings" do
-    fixture_path = Path.expand("fixtures", __DIR__)
-    file_path = Path.join(fixture_path, "zero_hex.txt")
-    read_bitstrings = CryptopalsElixir.FileReader.hex_file_to_bitstrings(file_path)
-    expected = [<<0::8>>]
+    test "one zero in file" do
+      fixture_path = Path.expand("fixtures", __DIR__)
+      file_path = Path.join(fixture_path, "zero_hex.txt")
+      read_bitstrings = CryptopalsElixir.FileReader.hex_file_to_bitstrings(file_path)
+      expected = [<<0::8>>]
 
-    assert(read_bitstrings == expected)
+      assert(read_bitstrings == expected)
+    end
+
+    test "one f in file" do
+      fixture_path = Path.expand("fixtures", __DIR__)
+      file_path = Path.join(fixture_path, "one_f_hex.txt")
+      read_bitstrings = CryptopalsElixir.FileReader.hex_file_to_bitstrings(file_path)
+    end
   end
 end
