ada2dbefec930bb1e6c77da4cbc39ad5da049f5f
Chris Jewell <chrisjohnjewell@gmail.com>
2018-10-19 14:07:32 +1100

    working

--- lib/cryptopals_elixir/file_reader.ex
+++ lib/cryptopals_elixir/file_reader.ex
@@ -1,9 +1,11 @@
 defmodule CryptopalsElixir.FileReader do
+  alias CryptopalsElixir.Conversion
+
   def hex_file_to_bitstrings(path) do
     path
     |> File.read!()
     |> String.trim()
     |> String.split("\n")
-    |> Enum.map(fn hex -> CryptopalsElixir.Conversion.hex_to_bitstring(hex) end)
+    |> Enum.map(&Conversion.hex_to_bitstring/1)
   end
 end
