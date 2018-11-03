21cca7db1bd32f4199346991e674e4fd7f5b9bef
Chris Jewell <chrisjohnjewell@gmail.com>
2018-11-03 17:24:02 +1100

    working

--- test/cryptopals_elixir/decryption_test.exs
+++ test/cryptopals_elixir/decryption_test.exs
@@ -141,4 +141,14 @@ defmodule CryptopalsElixir.DecryptionTest do
     assert(decrypted_string == original)
     assert(score == Decryption.score_xor_key(input, xor_key))
   end
+
+  test "decrypting one from a list" do
+    original =
+      "As Mr. Sloan always says, there is no \"I\" in team, but there is an \"I\" in pie. And there's an \"I\" in meat pie. Anagram of meat is team... I don't know what he's talking about."
+
+    xor_key = <<0b10101010::8>>
+    input = Encryption.xor_cypher(original, xor_key)
+
+    noise = for _ <- 0..80, into: "", do: <<Enum.random(0..255)::8>>
+  end
 end
