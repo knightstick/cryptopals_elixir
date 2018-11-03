6600aaa1c42023382816e04642524b5a12c96b6b
Chris Jewell <chrisjohnjewell@gmail.com>
2018-11-03 17:33:13 +1100

    working

--- lib/cryptopals_elixir.ex
+++ lib/cryptopals_elixir.ex
@@ -13,7 +13,7 @@ defmodule CryptopalsElixir do
     %{decrypted: decrypted, score: _score} =
       encrypted_hexstring
       |> CryptopalsElixir.Conversion.hex_to_bitstring()
-      |> CryptopalsElixir.Decryption.decrypt_xor_cypher_with_score()
+      |> CryptopalsElixir.Decryption.decrypt_xor_cypher()
 
     decrypted
   end
